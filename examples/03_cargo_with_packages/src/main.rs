769345b875a27060e3c8bc167c94605d2aced994
xing.he <1245036094@qq.com>
2021-12-10 16:59:07 +0800

    feat: update

---
+++ examples/03_cargo_with_packages/src/main.rs
@@ -0,0 +1,14 @@
+/**
+ * Cargo 官方包管理网址：https://crates.io/
+ *
+ * 可以使用命令行 `cargo search PACKAGE_NAME` 进行搜索
+ *
+ * 在 Cargo.toml 的 dependencies 中新增包，如：
+ *
+ * [dependencies]
+ * rand = "0.8.4"
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
